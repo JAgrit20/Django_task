a11606c047f2e505244b737184e151faaf9d441d
JAgrit20 <jagritacharya2020@gmail.com>
2022-09-21 12:38:42 +0530

    minor edits

--- django-task_app/tasks/views.py
+++ django-task_app/tasks/views.py
@@ -57,20 +57,3 @@ from django.views.generic import ListView, DetailView, \
 class TaskListView(ListView):
     model = Task
     context_object_name = 'tasks'
-
-class TaskDetailView(DetailView):
-    model = Task
-
-class TaskCreateView(CreateView):
-    model = Task
-    form_class = TaskForm
-    success_url = reverse_lazy('tasks:task_list')
-
-class TaskUpdateView(UpdateView):
-    model = Task
-    form_class = TaskForm
-    success_url = reverse_lazy('tasks:task_list')
-
-class TaskDeleteView(DeleteView):
-    model = Task
-    success_url = reverse_lazy('tasks:task_list')
